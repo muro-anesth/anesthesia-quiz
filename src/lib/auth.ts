fc8e1955de071c3468947eef74665b7af83449db
室内健志 <muro@shitsunaitakeshinoMac-mini.local>
2026-04-21 06:23:35 +0900

    use port 465 for Gmail

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,16 +8,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(db),
   providers: [
     Nodemailer({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: Number(process.env.EMAIL_SERVER_PORT ?? 587),
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
-    }),
+  server: {
+    host: process.env.EMAIL_SERVER_HOST,
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_SERVER_USER,
+      pass: process.env.EMAIL_SERVER_PASSWORD,
+    },
+  },
+  from: process.env.EMAIL_FROM,
+}),
   ],
   callbacks: {
     async signIn({ user }) {
